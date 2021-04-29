56368d5d2469f44ebc233f3c0b4eaf37175180b3
Timo Myllymäki <timo.myllymaki@digia.com>
2021-04-29 13:45:31 +0300

    feature/IISCRUM-1719 (#15)
    * IISCRUM-1719 One event per period, increase popup width
    
    * IISCRUM-1719 Fix table style

--- public/app/features/dashboard/components/DashNav/iiris_maintenance_modal.ts
+++ public/app/features/dashboard/components/DashNav/iiris_maintenance_modal.ts
@@ -551,6 +551,7 @@ export class IirisMaintenanceModalCtrl {
       y = this.yearInput.value;
       dayInputObject = this.dayInput;
     }
+    dayInputObject.options = [];
     if (m === 1 || m === 3 || m === 5 || m === 7 || m === 8 || m === 10 || m === 12) {
       maxAmount = 31;
     } else if (m === 4 || m === 6 || m === 9 || m === 11) {
